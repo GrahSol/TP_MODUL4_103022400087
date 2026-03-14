2f7f535f7430f1a3e0b61cbea261045b7e8431aa
GrahSol <anugrahpalesangpasinggi@gmail.com>
2026-03-15 02:29:42 +0700

    second commit

--- TP_MODUL4_103022400087/obj/Debug/net10.0/TP_MODUL4_103022400087.AssemblyInfo.cs
+++ TP_MODUL4_103022400087/obj/Debug/net10.0/TP_MODUL4_103022400087.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL4_103022400087")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda429e0ed0a5f8a4617aeb61b93c2568ffb9550")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL4_103022400087")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL4_103022400087")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
